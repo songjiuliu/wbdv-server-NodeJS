42a1fec2dde8f5f261972be537172b77f1d93318
songjiuliu <songjiuliu@hotmail.com>
2020-03-27 13:48:01 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
+    "https://boiling-bayou-51894.herokuapp.com");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
